a80005b2ab74372bc04f57e086d8717e0faa22cc
wallace <wallacegibbon@outlook.com>
2020-03-01 16:00:23 +0800

    .

--- calculator.erl
+++ calculator.erl
@@ -6,17 +6,21 @@ compile(ExprString) ->
     {ok, Tokens, _} = erl_scan:string(ExprString),
     {ok, [AST]} = erl_parse:parse_exprs(Tokens),
     Code = generate_code(AST),
-    io:format("Tokens: ~w~n~nAST: ~w~n~nCode: ~w~n", [Tokens, AST, Code]),
+    io:format("Tokens: ~w~n~nAST: ~p~n~nCode: ~w~n", [Tokens, AST, Code]),
     Code.
 
 generate_code({op, _Line, '+', Arg1, Arg2}) ->
     generate_code(Arg1) ++ generate_code(Arg2) ++ [add];
 generate_code({op, _Line, '*', Arg1, Arg2}) ->
     generate_code(Arg1) ++ generate_code(Arg2) ++ [mul];
+generate_code({op, _Line, '/', Arg1, Arg2}) ->
+    generate_code(Arg1) ++ generate_code(Arg2) ++ ['div'];
 generate_code({op, Line, Op, _, _}) ->
     exit_error("Unsupported operator in line ~w: ~w", [Line, Op]);
 generate_code({integer, _Line, I}) ->
-    [push, I].
+    [push, I];
+generate_code({float, _Line, F}) ->
+    [push, F].
 
 interpret([push, I | Rest], Stack) ->
     interpret(Rest, [I | Stack]);
@@ -24,6 +28,8 @@ interpret([add | Rest], [Arg2, Arg1 | Stack]) ->
     interpret(Rest, [Arg1 + Arg2 | Stack]);
 interpret([mul | Rest], [Arg2, Arg1 | Stack]) ->
     interpret(Rest, [Arg1 * Arg2 | Stack]);
+interpret(['div' | Rest], [Arg2, Arg1 | Stack]) ->
+    interpret(Rest, [Arg1 / Arg2 | Stack]);
 interpret([], [Rest | _]) ->
     Rest.
 
