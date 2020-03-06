c4512d8e59402f7e4c56369aca467bbcd12b6e31
wallace <wallacegibbon@outlook.com>
2020-03-06 16:29:08 +0800

    .

---
+++ otptest/name_server1.erl
@@ -0,0 +1,16 @@
+-module(name_server1).
+
+-export([init/0, add/2, find/1, handle/2, all_names/0, delete/1]).
+-import(server3, [rpc/2]).
+
+all_names() -> rpc(name_server, allNames).
+delete(Name) -> rpc(name_server, {delete, Name}).
+add(Name, Place) -> rpc(name_server, {add, Name, Place}).
+find(Name) -> rpc(name_server, {find, Name}).
+
+init() -> dict:new().
+handle({add, Name, Place}, Dict) -> {ok, dict:store(Name, Place, Dict)};
+handle({find, Name}, Dict) -> {dict:find(Name, Dict), Dict};
+handle({delete, Name}, Dict) -> {ok, dict:erase(Name, Dict)};
+handle(allNames, Dict) -> {dict:fetch_keys(Dict), Dict}.
+
