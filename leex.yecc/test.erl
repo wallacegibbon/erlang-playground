4d5dd780ae9dfde51cd4bfe2b516fcfd604f29c8
wallace <wallacegibbon@outlook.com>
2020-03-21 17:00:50 +0800

    .

---
+++ leex.yecc/test.erl
@@ -0,0 +1,11 @@
+-module(test).
+
+-compile(export_all).
+
+test() ->
+    {ok, Content} = file:read_file("./test.json"),
+    {ok, Tokens, _} = json_scan:string(binary_to_list(Content)),
+    {ok, Ast} = json_parse:parse(Tokens),
+    io:format("Tokens: ~w~nAst: ~p~n", [Tokens, Ast]),
+    ok.
+
