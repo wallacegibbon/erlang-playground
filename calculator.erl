d16265099ca01968c09c8eff69c060dfa4e07309
wallace <wallacegibbon@outlook.com>
2021-08-16 17:42:09 +0800

    The calculator.erl (stack machine) is updated and becomes more efficient and more readable

--- calculator.erl
+++ calculator.erl
@@ -1,27 +1,29 @@
 -module(calculator).
 
--compile(export_all).
+-export([generateCode/1, interpret/2, run/1]).
+
+interpret([push, I | Rest]  , Stack)                            -> interpret(Rest, [I | Stack]);
+interpret(['+'     | Rest]  , [Operand2, Operand1 | Stack])     -> interpret(Rest, [Operand1 + Operand2 | Stack]);
+interpret(['-'     | Rest]  , [Operand2, Operand1 | Stack])     -> interpret(Rest, [Operand1 - Operand2 | Stack]);
+interpret(['*'     | Rest]  , [Operand2, Operand1 | Stack])     -> interpret(Rest, [Operand1 * Operand2 | Stack]);
+interpret(['/'     | Rest]  , [Operand2, Operand1 | Stack])     -> interpret(Rest, [Operand1 / Operand2 | Stack]);
+interpret([]                , [Rest               | _])         -> Rest.
+
+generateCodeAndFlatten(Instructions) -> lists:flatten( generateCode(Instructions) ).
+
+generateCode({op, _Line, '+', Operand1, Operand2})              -> [generateCode(Operand1), generateCode(Operand2), '+'];
+generateCode({op, _Line, '-', Operand1, Operand2})              -> [generateCode(Operand1), generateCode(Operand2), '-'];
+generateCode({op, _Line, '*', Operand1, Operand2})              -> [generateCode(Operand1), generateCode(Operand2), '*'];
+generateCode({op, _Line, '/', Operand1, Operand2})              -> [generateCode(Operand1), generateCode(Operand2), '/'];
+generateCode({op,  Line, OP , _   , _})                         -> throw({unknownOperation, OP, Line});
+generateCode({integer,  _Line, Number})                         -> [push, Number];
+generateCode({float,    _Line, Number})                         -> [push, Number].
 
 compile(ExprString) ->
     {ok, Tokens, _} = erl_scan:string(ExprString),
     {ok, [AST]} = erl_parse:parse_exprs(Tokens),
-    Code = generateCode(AST),
+    Code = generateCodeAndFlatten(AST),
     io:format("Tokens: ~w~n~nAST: ~p~n~nCode: ~w~n", [Tokens, AST, Code]),
     Code.
 
-generateCode({op, _Line, '+', Arg1, Arg2})          -> generateCode(Arg1) ++ generateCode(Arg2) ++ [add];
-generateCode({op, _Line, '*', Arg1, Arg2})          -> generateCode(Arg1) ++ generateCode(Arg2) ++ [mul];
-generateCode({op, _Line, '/', Arg1, Arg2})          -> generateCode(Arg1) ++ generateCode(Arg2) ++ ['div'];
-generateCode({op, Line, Op, _, _})                  -> exit_error("Unsupported operator in line ~w: ~w", [Line, Op]);
-generateCode({integer, _Line, I})                   -> [push, I];
-generateCode({float, _Line, F})                     -> [push, F].
-
-interpret([push, I | Rest], Stack)                  -> interpret(Rest, [I | Stack]);
-interpret([add | Rest], [Arg2, Arg1 | Stack])       -> interpret(Rest, [Arg1 + Arg2 | Stack]);
-interpret([mul | Rest], [Arg2, Arg1 | Stack])       -> interpret(Rest, [Arg1 * Arg2 | Stack]);
-interpret(['div' | Rest], [Arg2, Arg1 | Stack])     -> interpret(Rest, [Arg1 / Arg2 | Stack]);
-interpret([], [Rest | _])                           -> Rest.
-
-exit_error(Fmt, Values) -> erlang:error(lists:flatten(io_lib:format(Fmt, Values))).
-
-run(ExprString) -> interpret(compile(ExprString), []).
+run(ExprString) -> interpret( compile(ExprString), [] ).
