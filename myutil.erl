d9a204d4fd76235e9ae493afde6c1cd6c29c2648
wallace <wallacegibbon@outlook.com>
2021-10-08 18:28:10 +0800

    A useful "compose" function for calling function in order (like the "|>" in elixir)

---
+++ myutil.erl
@@ -0,0 +1,18 @@
+-module(myutil).
+
+-export([compose/1]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+compose([Rule | Rules]) ->
+    fun (E) -> (compose(Rules))(Rule(E)) end;
+compose([]) ->
+    fun (E) -> E end.
+
+-ifdef(EUNIT).
+
+compose_test() ->
+    ComponsedFn = compose([fun (A) -> A + 1 end, fun (A) -> A * 2 end]),
+    ?assertEqual(8, ComponsedFn(3)).
+
+-endif.
