b91d86acf387317bea2220fd87e4d833772e2feb
wallace <wallacegibbon@outlook.com>
2022-01-21 18:41:05 +0800

    rbtree is not finished yet

--- matrix.erl
+++ matrix.erl
@@ -2,32 +2,37 @@
 
 -export([add_vv/2, mul_mm/2, mul_mv/2, mul_vs/2]).
 
-%% matrix - matrix multiplication
-mul_mm(M1, [V21 | V2R]) ->
-    [mul_mv(M1, V21) | mul_mm(M1, V2R)];
-mul_mm(_, []) ->
-    [].
+-type vector() :: [any()].
+-type matrix() :: [vector()].
 
-%% matrix - vector multiplication
-mul_mv(Matrix, Vector) ->
-    merge_vectors(mul_mv1(Matrix, Vector)).
+%% matrix * matrix
+-spec mul_mm(matrix(), matrix()) -> matrix().
+mul_mm(M1, M2) ->
+    lists:map(fun(V) -> mul_mv(M1, V) end, M2).
+
+%% matrix * vector
+-spec mul_mv(matrix(), vector()) -> vector().
+mul_mv(M, V) ->
+    merge_vectors(mul_mv1(M, V)).
 
 %% addition of a list of vectors
+-spec merge_vectors([vector()]) -> vector().
 merge_vectors([V1 | VR]) ->
     lists:foldl(fun add_vv/2, V1, VR).
 
+-spec mul_mv1(matrix(), vector()) -> [vector()].
 mul_mv1([MV | MVR], [E | ER]) ->
     [mul_vs(MV, E) | mul_mv1(MVR, ER)];
 mul_mv1([], []) ->
     [].
 
-%% vector - scalar multiplication
-mul_vs([E | R], S) ->
-    [E * S | mul_vs(R, S)];
-mul_vs([], _) ->
-    [].
+%% vector * scalar
+-spec mul_vs(vector(), integer()) -> vector().
+mul_vs(V, S) ->
+    lists:map(fun(E) -> E * S end, V).
 
-%% vector - vector addition
+%% vector + vector
+-spec add_vv(vector(), vector()) -> vector().
 add_vv([E1 | ER1], [E2 | ER2]) ->
     [E1 + E2 | add_vv(ER1, ER2)];
 add_vv([], []) ->
--- try_misc.erl
+++ try_misc.erl
@@ -2,33 +2,11 @@
 
 -compile(export_all).
 
-try_format() ->
-    io_lib:format("~c, ~.2f, ~s~n", [97, 3.2e+1, "hello"]).
-
-%io:format("~c, ~.2f, ~s~n", [97, 32.0, "hello"]).
-
 -spec factorial(pos_integer()) -> pos_integer().
 factorial(1) ->
     1;
 factorial(N) ->
     N * factorial(N - 1).
-
--spec cnt([any()]) -> pos_integer().
-cnt(S) ->
-    cnt(S, 0).
-
--spec cnt([any()], non_neg_integer()) -> pos_integer().
-cnt([_ | Rst], Cnt) ->
-    cnt(Rst, Cnt + 1);
-cnt([], Cnt) ->
-    Cnt.
-
-info(Name, [{Name, I} | _]) ->
-    I;
-info(Name, [_ | R]) ->
-    info(Name, R);
-info(_, []) ->
-    unknown.
 
 -spec qsort([number()]) -> [number()].
 qsort(Lst) ->
@@ -40,22 +18,13 @@ qsort1([P | T]) ->
 qsort1([]) ->
     [].
 
-%% this quick sort is much better, as it iterates the list only once.
+%% this one is much better, as it iterates the list only once during partition.
 -spec qsort2([number()]) -> [any()].
 qsort2([P | T]) ->
     {Left, Right} = lists:partition(fun(A) -> A < P end, T),
     [qsort2(Left), P, qsort2(Right)];
 qsort2([]) ->
     [].
-
--type a() :: string() | atom().
-
-%% neither of the following specs can make ret type same as arg type
-
-%-spec type_test(a()) -> a().
--spec type_test(A) -> A when A :: a().
-type_test(blah) ->
-    "blah".
 
 -record(blah, {a, b, c}).
 
